51abee69713c14bdd63ac1676235afd0de2bb08b
Pioneer <pioneerdev1023@gmail.com>
2022-01-20 16:19:28 +0300

    second commit

--- BasicGameJumper/Assets/Scripts/Player.cs
+++ BasicGameJumper/Assets/Scripts/Player.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform groundCheckTransform = null;
+    [SerializeField] private LayerMask playerMask;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // check if space key is pressed down
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
     }
+    
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    private void FixedUpdate()
+    {
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0) 
+        {
+            return;
+        }
+        if (jumpKeyWasPressed) 
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0); 
+    }
+
+    
 }

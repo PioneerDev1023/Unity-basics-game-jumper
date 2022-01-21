473578b838b53769cb84d68c4a1dc576a1ee84e5
Pioneer <pioneerdev1023@gmail.com>
2022-01-21 09:02:33 +0300

    finished_2022-01-21

--- BasicGameJumper/Assets/Scripts/Player.cs
+++ BasicGameJumper/Assets/Scripts/Player.cs
@@ -7,9 +7,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private Transform groundCheckTransform = null;
     [SerializeField] private LayerMask playerMask;
+
     private bool jumpKeyWasPressed;
     private float horizontalInput;
     private Rigidbody rigidbodyComponent;    
+    private int superJumpsRemaining;
     
     // Start is called before the first frame update
     void Start()
@@ -31,17 +33,34 @@ public class Player : MonoBehaviour
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     private void FixedUpdate()
     {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0); 
+
         if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0) 
         {
             return;
         }
         if (jumpKeyWasPressed) 
         {
-            rigidbodyComponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+            float jumpPower = 5f;
+            if (superJumpsRemaining > 0)
+            {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+            rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
             jumpKeyWasPressed = false;
         }
 
-        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y, 0); 
+        
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.layer == 7 )
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining ++;
+        }
     }
 
     
